d9dad3fbedd93b70a94da7115ddcf4b40f9ae28b
daeris admin <57998040+daerisadmin@users.noreply.github.com>
2022-07-22 12:25:22 +0200

    [FIX] stock: translations

--- addons/stock/models/stock_warehouse.py
+++ addons/stock/models/stock_warehouse.py
@@ -601,25 +601,25 @@ class Warehouse(models.Model):
                 'barcode': self._valid_barcode(code + '-STOCK', company_id)
             },
             'wh_input_stock_loc_id': {
-                'name': _('Input'),
+                'name': _('Entrada'),
                 'active': reception_steps != 'one_step',
                 'usage': 'internal',
                 'barcode': self._valid_barcode(code + '-INPUT', company_id)
             },
             'wh_qc_stock_loc_id': {
-                'name': _('Quality Control'),
+                'name': _('Control de Calidad'),
                 'active': reception_steps == 'three_steps',
                 'usage': 'internal',
                 'barcode': self._valid_barcode(code + '-QUALITY', company_id)
             },
             'wh_output_stock_loc_id': {
-                'name': _('Output'),
+                'name': _('Salida'),
                 'active': delivery_steps != 'ship_only',
                 'usage': 'internal',
                 'barcode': self._valid_barcode(code + '-OUTPUT', company_id)
             },
             'wh_pack_stock_loc_id': {
-                'name': _('Packing Zone'),
+                'name': _('Zona de Embalaje'),
                 'active': delivery_steps == 'pick_pack_ship',
                 'usage': 'internal',
                 'barcode': self._valid_barcode(code + '-PACKING', company_id)
